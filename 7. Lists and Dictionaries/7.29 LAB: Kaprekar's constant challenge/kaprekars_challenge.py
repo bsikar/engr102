d5069f2ed4e85477060e72de89e731743dfe0906
Brighton Sikarskie <bsikar@tuta.io>
2022-10-15 14:08:44 -0500

    unit 7

--- 7. Lists and Dictionaries/7.29 LAB: Kaprekar's constant challenge/kaprekars_challenge.py
+++ 7. Lists and Dictionaries/7.29 LAB: Kaprekar's constant challenge/kaprekars_challenge.py
@@ -17,9 +17,6 @@ def kaprekar(number):
     if len(set(str(number))) == 1 and len(str(number)) == 4:
         return 1
 
-    if number == "6174":
-        return 1
-
     count = 0
     while number != "6174":
         # Sort the digits in descending and then in ascending
@@ -41,7 +38,7 @@ def kaprekar(number):
 assert kaprekar("3524") == 3
 assert kaprekar("2111") == 5
 assert kaprekar("9831") == 7
-assert kaprekar("6174") == 1
+assert kaprekar("6174") == 0
 assert kaprekar("1111") == 1
 assert kaprekar("0007") == 4
 assert kaprekar("0000") == 1
